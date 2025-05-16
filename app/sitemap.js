0f1cc421e5effe8e746328221106af78928373b2
Mandionelaye <mandionelay99@gmail.com>
2025-05-16 13:24:45 +0000

    5.2 commit

--- app/sitemap.js
+++ app/sitemap.js
@@ -3,14 +3,17 @@ export default function sitemap() {
     {
       url: 'https://skillafrik-seven.vercel.app',
       lastModified: new Date(),
+       priority: 1,
     },
     {
-      url: 'https://skillafrik-seven.vercel.app/domaines',
+      url: 'https://skillafrik-seven.vercel.app/domaines/',
       lastModified: new Date(),
+       priority: 0.8,
     },
     {
-      url: 'https://skillafrik-seven.vercel.app/domaines/formation',
+      url: 'https://skillafrik-seven.vercel.app/domaines/formation/',
       lastModified: new Date(),
+      priority: 0.9,
     },
   ]
 }
